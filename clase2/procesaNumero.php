f64349181300c2b0f7c8168f5d13d77d0d0dae7e
marcos pinardi <exegeses@gmail.com>
2020-09-23 17:39:35 -0300

    condicional con dawto desde form

---
+++ clase2/procesaNumero.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/estilos.css">
+</head>
+<body>
+    <h1>Condicional capturando un dato desde form</h1>
+
+    <div class="objeto">
+<?php
+    $numero = $_POST['numero'];
+    //echo $numero;
+    if( $numero < 100 ){
+        echo 'el número es menor';
+    }
+    else{
+        echo 'el número NO es menor';
+    }
+?>        
+
+    </div>
+    <a href="enviaNumero.html">volver</a>
+
+</body>
+</html>
