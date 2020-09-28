c885b5a5b466e278b5e9ed3ffdcf789cd1aab297
marcos pinardi <exegeses@gmail.com>
2020-09-28 15:54:55 -0300

    teoria arrays

---
+++ clase3/arrays.php
@@ -0,0 +1,20 @@
+<?php
+    $marca[] = 'Nike';
+    $marca[] = 'Sony';
+    $marca[] = 'Puma';
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/estilos.css">
+</head>
+<body>
+    <h1>Arrays en PHP</h1>
+    <div class="objeto">
+        <?php echo $marca[2] ?>
+    </div>
+
+</body>
+</html>
