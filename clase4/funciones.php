a5e848ed4d4b23778ef71944464c44ae8b9f509b
marcos pinardi <exegeses@gmail.com>
2020-09-30 17:59:34 -0300

    tarea funciones

---
+++ clase4/funciones.php
@@ -0,0 +1,23 @@
+<?php
+
+    //declaración
+    function duplicar( $numero ){
+        $resultado = $numero * 2;
+        return $resultado;
+    }
+
+    //llamado a ejecución
+    echo duplicar( 15 );
+
+    //tarea:
+/*
+ * crear una funcion que divida dos números
+ *
+ * 1. ambos deben ser números
+ * 2. el divisor no puede ser 0
+ * 3. retornar resultado
+ *  (si se cumplen las dos anteriores)
+ *
+ *
+ * NOTA: no poner un echo dentro de la función
+ * */
