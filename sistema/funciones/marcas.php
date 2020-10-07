e3f0e384d2905feef9047c6193fffceacd4fe992
marcos pinardi <exegeses@gmail.com>
2020-10-07 16:29:36 -0300

    Panel de administración de marcas - listado

---
+++ sistema/funciones/marcas.php
@@ -0,0 +1,21 @@
+<?php
+
+    ########################
+    #### CRUD DE marcas ####
+
+    function listarMarcas()
+    {
+        $link = conectar();
+        $sql = "SELECT idMarca, mkNombre
+                    FROM marcas";
+        $resultado = mysqli_query( $link, $sql );
+        return $resultado;
+    }
+
+    /*
+     * listarMarcas()
+     * verMarcaPorID()
+     * agregarMarca()
+     * modificarMarca()
+     * eliminarMarca()
+     * */
