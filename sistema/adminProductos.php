013728c77f55f096e0d94f13487c9a5936dc75c5
marcos pinardi <exegeses@gmail.com>
2020-10-07 17:34:13 -0300

    Panle de administración de productos - listado

---
+++ sistema/adminProductos.php
@@ -0,0 +1,67 @@
+<?php  
+
+    require 'funciones/conexion.php';
+    require 'funciones/productos.php';
+    $productos = listarProductos();
+	include 'includes/header.html';
+	include 'includes/nav.php';  
+?>
+
+    <main class="container">
+        <h1>Panel de administración de productos</h1>
+
+        <a href="admin.php" class="btn btn-outline-secondary mb-3">
+            Volver a dashboard
+        </a>
+
+        <table class="table table-bordered table-stripped table-hover">
+            <thead class="thead-dark">
+            <tr>
+                <th>Producto</th>
+                <th>Precio</th>
+                <th>Marca</th>
+                <th>Categoría</th>
+                <th>Presentacíon</th>
+                <th>Imagen</th>
+                <th colspan="2">
+                    <a href="" class="btn btn-dark">
+                        Agregar
+                    </a>
+                </th>
+            </tr>
+            </thead>
+            <tbody>
+<?php
+        while( $producto = mysqli_fetch_assoc($productos) ){
+?>
+            <tr>
+                <td><?= $producto['prdNombre'] ?></td>
+                <td><?= $producto['prdPrecio'] ?></td>
+                <td><?= $producto['mkNombre'] ?></td>
+                <td><?= $producto['catNombre'] ?></td>
+                <td><?= $producto['prdPresentacion'] ?></td>
+                <td><?= $producto['prdImagen'] ?></td>
+                <td>
+                    <a href="" class="btn btn-outline-secondary">
+                        Modificar
+                    </a>
+                </td>
+                <td>
+                    <a href="" class="btn btn-outline-secondary">
+                        Eliminar
+                    </a>
+                </td>
+            </tr>
+<?php
+        }
+?>
+            </tbody>
+        </table>
+
+        <a href="admin.php" class="btn btn-outline-secondary">
+            Volver a dashboard
+        </a>
+
+    </main>
+
+<?php  include 'includes/footer.php';  ?>
---
+++ sistema/funciones/productos.php
@@ -0,0 +1,18 @@
+<?php
+
+    #############################
+    ##### CRUD DE Productos #####
+
+    function listarProductos()
+    {
+        $link = conectar();
+        $sql = "SELECT 
+                        p.idProducto, p.prdNombre, p.prdPrecio, 
+                        m.mkNombre, c.catNombre, 
+                        p.prdPresentacion, p.prdImagen 
+                    FROM productos p 
+                    INNER JOIN marcas m ON p.idMarca = m.idMarca 
+                    INNER JOIN categorias c ON p.idCategoria = c.idCategoria";
+        $resultado = mysqli_query( $link, $sql );
+        return $resultado;
+    }
