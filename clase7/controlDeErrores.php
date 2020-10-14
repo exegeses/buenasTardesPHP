d5c2962d9c33464015d8892c71397f7873f44e5c
marcos pinardi <exegeses@gmail.com>
2020-10-14 17:52:01 -0300

    teoría de control de errores

---
+++ clase7/controlDeErrores.php
@@ -0,0 +1,33 @@
+<?php
+    $link = mysqli_connect(
+                'localhost',
+                'root',
+                'root',
+                'catalogo'
+            );
+    $sql = "SELECT idCategoria, catNombre
+                FROM categorias";
+    $categorias = mysqli_query($link, $sql)
+                    or die( mysqli_error($link) );
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/estilos.css">
+</head>
+<body>
+    <h1>Listado de categorías</h1>
+    <div class="objeto">
+        <?php
+            while( $categoria = mysqli_fetch_assoc($categorias) ){
+                echo $categoria['idCategoria'], ' ';
+                echo $categoria['catNombre'], '<br>';
+            }
+        ?>
+    </div>
+
+    ok
+</body>
+</html>
