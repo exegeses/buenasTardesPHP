267f83a44d81c14fe7d008e9f5504b1650ab4be9
marcos pinardi <exegeses@gmail.com>
2020-10-28 17:50:34 -0300

    teoría de sesiones

---
+++ clase11/crearSesion.php
@@ -0,0 +1,10 @@
+<?php
+
+    //directiva de sesión
+    session_start();
+
+    //registramos variables de sesion
+    $_SESSION['nombre'] = 'marcos';
+    $_SESSION['numero'] = 666;
+
+    //nombre|s:6:"marcos";numero|i:666;
